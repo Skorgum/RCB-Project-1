b5bd4f26abd68682d3b70cec4bbfe51e06a933b5
noelru94 <noelru94@gmail.com>
2019-07-13 14:04:47 -0400

    search function envokes an ajax request to ticketmaster api

--- app.js
+++ app.js
@@ -16,9 +16,45 @@ var firebaseConfig = {
 // global variables
 
 // onclick event for search
-$('#search').on('click',function(event){
-  event.preventDefault()
-  console.log("click");
-})
+$('#search').on('click',search);
+
+
+
+ function search(event){
+  event.preventDefault();
+
+  var location = $('#location').val();
+  var event = $('#event').val();
+  var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + event + "&apikey=jMH0oHQBw8sCR7SRnBKCRRoG6gn2I5S8";
+
+  $.ajax({
+      url: "http://104.200.17.235:8081/cors/",
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify({
+      url: queryURL
+      })
+  }).then(function (response) {
+
+      var data = JSON.parse(response);
+      var eventArray = data._embedded.events;
+      console.log(eventArray);
+       
+          for (var i= 0; i < eventArray.length; i++) {  
+          var event = eventArray[i];
+          var name = event.name;
+          console.log(name);
+          var ticketURL = event.url;
+          console.log(ticketURL);
+          var title = $(`<a href="${ticketURL}">${name}</a>`)                                                                 
+          var eventDiv = $("<div class='name'>");
+          eventDiv.append(title);
+          $("#demo-api").append(eventDiv);
+          }
+          
+  })
+ 
+};
+
 
 // API
