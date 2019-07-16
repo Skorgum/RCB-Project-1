379648ded9c421326cabbaae234738a2020fd5ee
noelru94 <noelru94@gmail.com>
2019-07-16 19:22:32 -0400

    update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var firebaseConfig = {
 // global variables
 
 // onclick event for search
-$(document).on('click','#search',search);
+$('#search').on('click',search);
 
 
 
@@ -28,48 +28,41 @@ $(document).on('click','#search',search);
   var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + event + "&apikey=jMH0oHQBw8sCR7SRnBKCRRoG6gn2I5S8";
 
   $.ajax({
-      url: "http://104.200.17.235:8081/cors/",
-      method: "POST",
-      contentType: "application/json",
-      data: JSON.stringify({
-      url: queryURL
-      })
+    url: "http://104.200.17.235:8081/cors/",
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({
+    url: queryURL
+    })
   }).then(function (response) {
 
       var data = JSON.parse(response);
       var eventArray = data._embedded.events;
       console.log(eventArray);
        
-          for (var i= 0; i < eventArray.length; i++) {  
-            var event = eventArray[i];
-            var name = event.name;
-            var imgURL = event.images[0].url
-            var img = $(`<img src="${imgURL}" width="100" height="100">`)
-            console.log(name);
-            var ticketURL = event.url;
-            console.log(ticketURL);
-            var title = $(`<a href="${ticketURL}">${name}</a>`)                                                                 
-            var eventsDiv = $('<div class="events">');
-            var eventDiv = 
-            $(`<div class="row event-row">
-                  <div class="col-md-2">
-                    <img src="${imgURL}" width="100" height="100">
-                  </div>
-                  <div class="col-md-10">
-                    <div class"card">
-                      <div class="card-body">
-                        <h5 class="card-title"><a href="${ticketURL}">${name}</a></h5>
-                        <p class ="card-text">Some quick example text to build on the card title and make</p>
-                        <a href="${ticketURL}" id="tickets" class="btn">Tickets</a>
-                      </div>
-                  </div>
-                </div> 
-              `)
-            // eventDiv.append(title);
-            $(eventsDiv).append(eventDiv);
-            $('#events').append(eventsDiv);
+        for (var i= 0; i < eventArray.length; i++) {  
+          var event = eventArray[i];
+          var eventTitle = event.name;
+          var imgURL = event.images[0].url
+          var TicketMaster = event.url;                                                              
+          var eventsSection = $('<section class="row events">');
+          var eventDiv = 
+          // eventDiv includes image of event with info to the right set in a card
+          $(`<div class="col-sm-12 col-md-2">
+                <img src="${imgURL}>
+            </div>
+            <div class="col-md-10 col-sm-12">
+              <div class"card">
+                <div class="card-body">
+                  <h5 class="card-title"><a href="${TicketMaster}">${eventTitle}</a></h5>
+                  <p class ="card-text">Some quick example text to build on the card title and make</p>
+                  <a href="${TicketMaster}" id="tickets" class="btn">Tickets</a>
+                </div>
+              </div>
+            </div>`);
 
-            // $('#demo-api').append(img);
+            $(eventsSection).append(eventDiv);
+            $('#events-list').append(eventsSection);
           }
           
   })
