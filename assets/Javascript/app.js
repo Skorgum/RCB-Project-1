aa46de18d7054ca073cddad31209088f264bc2eb
carlitos <cmmurillo1@hotmail.com>
2019-07-18 21:06:53 -0400

    java

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -2,86 +2,82 @@ console.log("(⌐■_■)");
 
 // Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCCJzvIcPUPTcn5jsc1T727-OWc3AittmM",
-    authDomain: "event-meta-search.firebaseapp.com",
-    databaseURL: "https://event-meta-search.firebaseio.com",
-    projectId: "event-meta-search",
-    storageBucket: "",
-    messagingSenderId: "650900915826",
-    appId: "1:650900915826:web:919356c789cac91a"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  var dataRef = firebase.database();
-  var name = "";
-  var email = "";
-  $("#btn btn-primary").on("click", function(event) {
-    event.preventDefault();
+  apiKey: "AIzaSyCCJzvIcPUPTcn5jsc1T727-OWc3AittmM",
+  authDomain: "event-meta-search.firebaseapp.com",
+  databaseURL: "https://event-meta-search.firebaseio.com",
+  projectId: "event-meta-search",
+  storageBucket: "",
+  messagingSenderId: "650900915826",
+  appId: "1:650900915826:web:919356c789cac91a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var dataRef = firebase.database();
+var name = "";
+var email = "";
+$("#btn btn-primary").on("click", function (event) {
+  event.preventDefault();
 
-  
+  name = $("#inputName").val().trim();
+  email = $("#inputEmail").val().trim();
 
 
-    
-    name = $("#inputName").val().trim();
-    email = $("#inputEmail").val().trim();
-  
+  // Code for the push
+  dataRef.ref().push({
 
-    // Code for the push
-    dataRef.ref().push({
+    name: name,
+    email: email,
 
-      name: name,
-      email: email,
-      
   });
-// global variables
+  // global variables
 
-// onclick event for search
-$(document).on('click','#search',search);
-
-
+  // onclick event for search
+  $(document).on('click', '#search', search);
 
 
- function search(e){
 
-  e.preventDefault();
 
-  $('.event-row').remove();
+  function search(e) {
 
-  var location = $('#location').val();
-  var event = $('#event').val();
-  var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + event + "&apikey=jMH0oHQBw8sCR7SRnBKCRRoG6gn2I5S8";
+    e.preventDefault();
+
+    $('.event-row').remove();
+
+    var location = $('#location').val();
+    var event = $('#event').val();
+    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + event + "&apikey=jMH0oHQBw8sCR7SRnBKCRRoG6gn2I5S8";
 
 
     $.ajax({
-        url: 'https://cors-anywhere.herokuapp.com/' + queryURL,
-        headers: { 'X-Requested-With': 'XMLHttpRequest' },
-        method: "GET",
-        contentType: "application/json",
+      url: 'https://cors-anywhere.herokuapp.com/' + queryURL,
+      headers: { 'X-Requested-With': 'XMLHttpRequest' },
+      method: "GET",
+      contentType: "application/json",
 
-    }).then(function (response){
+    }).then(function (response) {
 
-        var data = response;
-        var eventArray = data._embedded.events;
-        console.log(eventArray);
-        
-        for (var i= 0; i < eventArray.length; i++) {  
-          var event = eventArray[i];
-          var name = event.name;
-          var imgURL = event.images[0].url
-          var ticketURL = event.url;
-          var city = event._embedded.venues[0].city.name;
-          function weather(){// weather data for each event location
-            $.ajax({
-              url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" +
-                    "&APPID=1b7d73dc9e11e1001798c791a1226534",
-              type: "GET",
-              }).then(function(response){
-                 console.log(response)
-                })
-          };
-          weather();                                                               
-          var eventsDiv = $('<div class="events">');
-          var eventDiv = 
+      var data = response;
+      var eventArray = data._embedded.events;
+      console.log(eventArray);
+
+      for (var i = 0; i < eventArray.length; i++) {
+        var event = eventArray[i];
+        var name = event.name;
+        var imgURL = event.images[0].url
+        var ticketURL = event.url;
+        var city = event._embedded.venues[0].city.name;
+        function weather() {// weather data for each event location
+          $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" +
+              "&APPID=1b7d73dc9e11e1001798c791a1226534",
+            type: "GET",
+          }).then(function (response) {
+            console.log(response)
+          })
+        };
+        weather();
+        var eventsDiv = $('<div class="events">');
+        var eventDiv =
           $(`<div class="row event-row">
                 <div class="col-md-2">
 
@@ -96,11 +92,11 @@ $(document).on('click','#search',search);
                     </div>
                 </div>
               </div>`)
-              $(eventsDiv).append(eventDiv);
-              $('#events').append(eventsDiv);
-        }          
+        $(eventsDiv).append(eventDiv);
+        $('#events').append(eventsDiv);
+      }
     })
-};
+  };
 
 
 
@@ -114,11 +110,11 @@ $(document).on('click','#search',search);
   //         console.log(response)
   //         console.log('weather')
   //       })
-  
+
   // };
 
 // function showWeather(data){
- 
+
 
 //          <h3><strong>Description</strong>: "+ data.weather[0].description +"</h3>" +
 //          "<h3><strong>Temperature</strong>: "+ data.main.temp +"</h3>" +
